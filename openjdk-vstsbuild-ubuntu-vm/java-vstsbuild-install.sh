d6055991cb01ccab410966bce921e9471a06b699
U-REDMOND\pabarham <pabarham@pbarham-laptop.redmond.corp.microsoft.com>
2016-06-08 08:59:20 -0400

    Updated script to place downloads in user home directory and runas the user (not as root)

--- openjdk-vstsbuild-ubuntu-vm/java-vstsbuild-install.sh
+++ openjdk-vstsbuild-ubuntu-vm/java-vstsbuild-install.sh
@@ -32,14 +32,14 @@ sudo apt-get -y install gradle
 
 sudo apt-get -y install libunwind8 libcurl3
 
-mkdir ~/downloads
-cd ~/downloads
-wget https://github.com/Microsoft/vsts-agent/releases/download/v2.101.1/vsts-agent-ubuntu.14.04-x64-2.101.1.tar.gz
-wget http://security.ubuntu.com/ubuntu/pool/main/i/icu/libicu52_52.1-8ubuntu0.2_amd64.deb
+sudo -u $5 mkdir /home/$5/downloads
+cd /home/$5/downloads
+sudo -u $5 wget https://github.com/Microsoft/vsts-agent/releases/download/v2.101.1/vsts-agent-ubuntu.14.04-x64-2.101.1.tar.gz
+sudo -u $5 wget http://security.ubuntu.com/ubuntu/pool/main/i/icu/libicu52_52.1-8ubuntu0.2_amd64.deb
 sudo dpkg -i libicu52_52.1-8ubuntu0.2_amd64.deb
 
-mkdir ~/vsts-agent
-cd ~/vsts-agent
-tar xzf ~/downloads/vsts-agent-ubuntu*
+sudo -u $5 mkdir /home/$5/vsts-agent
+cd /home/$5/vsts-agent
+sudo -u $5 tar xzf /home/$5/downloads/vsts-agent-ubuntu*
 sudo bin/Agent.Listener --unattended --runasservice --replace --acceptteeeula --url $1 --auth PAT --token $2 --pool $3 --agent $4
 
