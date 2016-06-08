7dd441b01e3fd6158183a984c03a7b955bbd5dcb
U-REDMOND\pabarham <pabarham@pbarham-laptop.redmond.corp.microsoft.com>
2016-06-08 15:25:00 -0400

    Edited the script -- now works and tested

--- openjdk-vstsbuild-ubuntu-vm/java-vstsbuild-install.sh
+++ openjdk-vstsbuild-ubuntu-vm/java-vstsbuild-install.sh
@@ -1,7 +1,13 @@
-#/bin/sh
+#!/bin/sh
+
+
+
 #Install Build Tools
+
 sudo apt-get -y install build-essential
 sudo apt-get -y install packaging-dev
+
+
 
 #Install Java
 
@@ -25,21 +31,46 @@ sudo apt-get install -y openjdk-8-jdk
 # sudo apt-get -y update
 # sudo apt-get install -y oracle-java8-installer
 
+
+
 #Install Java build tools
+
 sudo apt-get -y install ant
 sudo apt-get -y install maven
 sudo apt-get -y install gradle
 
+
+
+# Install VSTS build agent dependencies
 sudo apt-get -y install libunwind8 libcurl3
 
+
+
+# Download VSTS build agent and required security patch
 sudo -u $5 mkdir /home/$5/downloads
 cd /home/$5/downloads
 sudo -u $5 wget https://github.com/Microsoft/vsts-agent/releases/download/v2.101.1/vsts-agent-ubuntu.14.04-x64-2.101.1.tar.gz
 sudo -u $5 wget http://security.ubuntu.com/ubuntu/pool/main/i/icu/libicu52_52.1-8ubuntu0.2_amd64.deb
 sudo dpkg -i libicu52_52.1-8ubuntu0.2_amd64.deb
 
+
+
+# Install VSTS agent
 sudo -u $5 mkdir /home/$5/vsts-agent
 cd /home/$5/vsts-agent
 sudo -u $5 tar xzf /home/$5/downloads/vsts-agent-ubuntu*
-sudo bin/Agent.Listener --unattended --runasservice --replace --acceptteeeula --url $1 --auth PAT --token $2 --pool $3 --agent $4
+# sudo bin/Agent.Listener --unattended --runasservice --replace --acceptteeeula --url $1 --auth PAT --token $2 --pool $3 --agent $4
 
+echo "LANG=en_US.UTF-8" > .Env
+echo "JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64" >> .Env
+# export JAVA_HOME_6_X64=/usr/lib/jvm/java-6-oracle
+echo "JAVA_HOME_6_X64=/usr/lib/jvm/java-6-openjdk-amd64" >> .Env
+# export JAVA_HOME_7_X64=/usr/lib/jvm/java-7-oracle
+echo "JAVA_HOME_7_X64=/usr/lib/jvm/java-7-openjdk-amd64" >> .Env
+# export JAVA_HOME_8_X64=/usr/lib/jvm/java-8-oracle
+echo "JAVA_HOME_8_X64=/usr/lib/jvm/java-8-openjdk-amd64" >> .Env
+
+# sudo -u $5 -E sh ./config.sh --unattended --runasservice --replace --acceptteeeula --url $1 --auth PAT --token $2 --pool $3 --agent $4
+# source ./config.sh --unattended --runasservice --replace --acceptteeeula --url $1 --auth PAT --token $2 --pool $3 --agent $4
+
+sudo bin/Agent.Listener --configure --unattended --runasservice --replace --acceptteeeula --url $1 --auth PAT --token $2 --pool $3 --agent $4
