90c23fe76dd9f670276b4ddaa0c2ad6d423fa83f
U-REDMOND\pabarham <pabarham@pbarham-laptop.redmond.corp.microsoft.com>
2016-06-09 11:14:10 -0400

    Now download and install NodeJS, npm and gulp. Fixed env-var for NodeJS. Echo progress to user home directory.

--- openjdk-vstsbuild-ubuntu-vm/java-vstsbuild-install.sh
+++ openjdk-vstsbuild-ubuntu-vm/java-vstsbuild-install.sh
@@ -4,18 +4,25 @@
 
 #Install Build Tools
 
+echo "Installing build-essential package" > /home/$5/install.progress.txt
 sudo apt-get -y install build-essential
+
+echo "Installing packaging-dev package" >> /home/$5/install.progress.txt
 sudo apt-get -y install packaging-dev
 
 
 
 #Install Java
 
+echo "Installing openjdk-7-jdk package" >> /home/$5/install.progress.txt
+
 sudo add-apt-repository -y ppa:openjdk-r/ppa
 sudo apt-get -y update
 sudo apt-get install -y openjdk-7-jdk
 sudo apt-get -y update --fix-missing
 sudo apt-get install -y openjdk-7-jdk
+
+echo "Installing openjdk-8-jdk package" >> /home/$5/install.progress.txt
 
 sudo add-apt-repository -y ppa:openjdk-r/ppa
 sudo apt-get -y update
@@ -36,17 +43,32 @@ sudo apt-get install -y openjdk-8-jdk
 #Install Java build tools
 
 sudo apt-get -y install ant
+echo "Installing ant package" >> /home/$5/install.progress.txt
 sudo apt-get -y install maven
+echo "Installing maven package" >> /home/$5/install.progress.txt
 sudo apt-get -y install gradle
+echo "Installing gradle package" >> /home/$5/install.progress.txt
+
+# Install NodeJS, npm, and gulp
+echo "Installing NodeJS package" >> /home/$5/install.progress.txt
+sudo curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get install -y nodejs
+echo "Installing gulp package" >> /home/$5/install.progress.txt
+sudo npm install -g gulp
+sudo npm install gulp --save-dev
 
 
 
 # Install VSTS build agent dependencies
+
+echo "Installing libunwind8 and libcurl3 package" >> /home/$5/install.progress.txt
 sudo apt-get -y install libunwind8 libcurl3
 
 
 
 # Download VSTS build agent and required security patch
+
+echo "Installing VSTS Build agent package" >> /home/$5/install.progress.txt
 sudo -u $5 mkdir /home/$5/downloads
 cd /home/$5/downloads
 sudo -u $5 wget https://github.com/Microsoft/vsts-agent/releases/download/v2.101.1/vsts-agent-ubuntu.14.04-x64-2.101.1.tar.gz
@@ -70,7 +92,13 @@ echo "JAVA_HOME_7_X64=/usr/lib/jvm/java-7-openjdk-amd64" >> .Env
 # export JAVA_HOME_8_X64=/usr/lib/jvm/java-8-oracle
 echo "JAVA_HOME_8_X64=/usr/lib/jvm/java-8-openjdk-amd64" >> .Env
 
+# Remove NODE_ENV=production from service template file
+sudo sed -i 's,NODE_ENV=production,,g' ./bin/vsts.agent.service.template
+
 # sudo -u $5 -E sh ./config.sh --unattended --runasservice --replace --acceptteeeula --url $1 --auth PAT --token $2 --pool $3 --agent $4
 # source ./config.sh --unattended --runasservice --replace --acceptteeeula --url $1 --auth PAT --token $2 --pool $3 --agent $4
 
 sudo bin/Agent.Listener --configure --unattended --runasservice --replace --acceptteeeula --url $1 --auth PAT --token $2 --pool $3 --agent $4
+
+echo "ALL DONE!" >> /home/$5/install.progress.txt
+
