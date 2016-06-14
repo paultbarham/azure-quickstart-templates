5e2c8093615aae7097afd04a7b75a46176a8426e
U-REDMOND\pabarham <pabarham@pbarham-laptop.redmond.corp.microsoft.com>
2016-06-14 10:13:32 -0400

    Added all the commands necessary to install Oracle Java 6, 7, and 8.  Added commands to install vsce (for VS Code), and install all of our Eclipse and IntelliJ libs needed to build IDE plugins for VSTS.

--- vsts-idebuild-ubunta-vm/java-vstsbuild-install.sh
+++ vsts-idebuild-ubunta-vm/java-vstsbuild-install.sh
@@ -17,16 +17,43 @@ echo "Done." >> /home/$5/install.progress.txt
 # Install Java
 
 sudo /bin/date +%H:%M:%S >> /home/$5/install.progress.txt
-echo "Installing openjdk-7-jdk package" >> /home/$5/install.progress.txt
+echo "Installing Oracle Java 6 package" >> /home/$5/install.progress.txt
 
+# Install Oracle Java in silent mode
+echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
+echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
 
-sudo add-apt-repository -y ppa:openjdk-r/ppa
+sudo add-apt-repository -y ppa:webupd8team/java
 sudo apt-get -y update
-sudo apt-get install -y openjdk-7-jdk
-sudo apt-get -y update --fix-missing
-sudo apt-get install -y openjdk-7-jdk
+sudo apt-get install -y oracle-java6-installer
+
 sudo /bin/date +%H:%M:%S >> /home/$5/install.progress.txt
 echo "Done." >> /home/$5/install.progress.txt
+
+echo "Installing Oracle Java 7 package" >> /home/$5/install.progress.txt
+
+sudo apt-get install -y oracle-java7-installer
+
+sudo /bin/date +%H:%M:%S >> /home/$5/install.progress.txt
+echo "Done." >> /home/$5/install.progress.txt
+
+echo "Installing Oracle Java 8 package" >> /home/$5/install.progress.txt
+
+sudo apt-get install -y oracle-java8-installer
+
+sudo /bin/date +%H:%M:%S >> /home/$5/install.progress.txt
+echo "Done." >> /home/$5/install.progress.txt
+
+# sudo /bin/date +%H:%M:%S >> /home/$5/install.progress.txt
+# echo "Installing openjdk-7-jdk package" >> /home/$5/install.progress.txt
+
+# sudo add-apt-repository -y ppa:openjdk-r/ppa
+# sudo apt-get -y update
+# sudo apt-get install -y openjdk-7-jdk
+# sudo apt-get -y update --fix-missing
+# sudo apt-get install -y openjdk-7-jdk
+# sudo /bin/date +%H:%M:%S >> /home/$5/install.progress.txt
+# echo "Done." >> /home/$5/install.progress.txt
 
 sudo /bin/date +%H:%M:%S >> /home/$5/install.progress.txt
 echo "Installing openjdk-8-jdk package" >> /home/$5/install.progress.txt
@@ -39,19 +66,6 @@ sudo apt-get install -y openjdk-8-jdk
 
 sudo /bin/date +%H:%M:%S >> /home/$5/install.progress.txt
 echo "Done." >> /home/$5/install.progress.txt
-
-# Install Oracle Java in silent mode
-# echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
-# echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
-
-# sudo add-apt-repository -y ppa:webupd8team/java
-# sudo apt-get -y update
-# sudo apt-get install -y oracle-java7-installer
-
-# sudo add-apt-repository -y ppa:webupd8team/java
-# sudo apt-get -y update
-# sudo apt-get install -y oracle-java8-installer
-
 
 
 # Install Java build tools
@@ -89,6 +103,12 @@ sudo -u $5 npm install gulp --save-dev
 sudo /bin/date +%H:%M:%S >> /home/$5/install.progress.txt
 echo "Done." >> /home/$5/install.progress.txt
 
+sudo /bin/date +%H:%M:%S >> /home/$5/install.progress.txt
+echo "Installing vsce package" >> /home/$5/install.progress.txt
+sudo npm install -g vsce
+sudo /bin/date +%H:%M:%S >> /home/$5/install.progress.txt
+echo "Done." >> /home/$5/install.progress.txt
+
 
 # Install VSTS build agent dependencies
 
@@ -102,7 +122,7 @@ echo "Done." >> /home/$5/install.progress.txt
 # Download VSTS build agent and required security patch
 
 sudo /bin/date +%H:%M:%S >> /home/$5/install.progress.txt
-echo "Installing VSTS Build agent package" >> /home/$5/install.progress.txt
+echo "Downloading VSTS Build agent package" >> /home/$5/install.progress.txt
 
 sudo -u $5 mkdir /home/$5/downloads
 cd /home/$5/downloads
@@ -111,20 +131,49 @@ sudo -u $5 wget https://github.com/Microsoft/vsts-agent/releases/download/v2.101
 sudo -u $5 wget http://security.ubuntu.com/ubuntu/pool/main/i/icu/libicu52_52.1-8ubuntu0.2_amd64.deb
 sudo dpkg -i libicu52_52.1-8ubuntu0.2_amd64.deb
 
+sudo /bin/date +%H:%M:%S >> /home/$5/install.progress.txt
+echo "Done." >> /home/$5/install.progress.txt
+
+sudo /bin/date +%H:%M:%S >> /home/$5/install.progress.txt
+echo "Downloading and Installing VSTS IDE lib dependencies" >> /home/$5/install.progress.txt
 
 # Download Team needed libraries
-# sudo -u $5 wget https://xplatalm.blob.core.windows.net/buildlibs/git4idea.zip
-# sudo -u $5 wget https://xplatalm.blob.core.windows.net/buildlibs/eclipse.egit.repository-2.1.0.201209190230-r.zip
-# sudo -u $5 wget https://xplatalm.blob.core.windows.net/buildlibs/eclipse-SDK-3.5.2-linux-gtk-x86_64.tar.gz
-# sudo -u $5 wget https://xplatalm.blob.core.windows.net/buildlibs/ideaIC-14.1.4.zip
-# sudo -u $5 wget https://xplatalm.blob.core.windows.net/buildlibs/ideaIC-15.0.6.tar.gz
-# sudo -u $5 wget https://xplatalm.blob.core.windows.net/buildlibs/ideaIC-2016.1.2b.tar.gz
+sudo -u $5 wget https://xplatalm.blob.core.windows.net/buildlibs/git4idea.zip
+sudo -u $5 wget https://xplatalm.blob.core.windows.net/buildlibs/eclipse.egit.repository-2.1.0.201209190230-r.zip
+sudo -u $5 wget https://xplatalm.blob.core.windows.net/buildlibs/eclipse-SDK-3.5.2-linux-gtk-x86_64.tar.gz
+sudo -u $5 wget https://xplatalm.blob.core.windows.net/buildlibs/ideaIC-14.1.4.zip
+sudo -u $5 wget https://xplatalm.blob.core.windows.net/buildlibs/ideaIC-15.0.6.tar.gz
+sudo -u $5 wget https://xplatalm.blob.core.windows.net/buildlibs/ideaIC-2016.1.2b.tar.gz
+
+sudo -u $5 mkdir /home/$5/libs
+cd /home/$5/libs
+
+sudo -u $5 tar xvf /home/$5/downloads/eclipse-SDK-3.5.2-linux-gtk-x86_64.tar.gz
+sudo -u $5 mv eclipse eclipse-SDK-3.5.2
+cd eclipse-SDK-3.5.2
+sudo -u $5 unzip /home/$5/downloads/eclipse.egit.repository-2.1.0.201209190230-r.zip
+
+cd /home/$5/libs
+sudo -u $5 unzip /home/$5/downloads/ideaIC-14.1.4.zip
+sudo -u $5 tar xvf /home/$5/downloads/ideaIC-15.0.6.tar.gz
+sudo -u $5 tar xvf /home/$5/downloads/ideaIC-2016.1.2b.tar.gz
+sudo -u $5 ln -s idea-14.1.4 idea
+sudo -u $5 unzip /home/$5/downloads/git4idea.zip
+sudo -u $5 mkdir idea-14.1.4/plugins/
+sudo -u $5 mkdir idea-14.1.4/plugins/git4idea/
+sudo -u $5 mkdir idea-14.1.4/plugins/git4idea/lib
+sudo -u $5 mv git4idea/git4idea.jar idea-14.1.4/plugins/git4idea/lib/
 
 # Configure Team gradle properties
-# touch /home/$5/.gradle/gradle.properties
-# echo "ideaSdk=/home/"$5"/lib/idea/lib" >> /home/$5/.gradle/gradle.properties
-# echo "git4idea=/home/"$5"/lib/idea/plugins/git4idea/lib" >> /home/$5/.gradle/gradle.properties
+touch /home/$5/.gradle/gradle.properties
+echo "ideaSdk=/home/"$5"/lib/idea/lib" >> /home/$5/.gradle/gradle.properties
+echo "git4idea=/home/"$5"/lib/idea/plugins/git4idea/lib" >> /home/$5/.gradle/gradle.properties
 
+sudo /bin/date +%H:%M:%S >> /home/$5/install.progress.txt
+echo "Done." >> /home/$5/install.progress.txt
+
+sudo /bin/date +%H:%M:%S >> /home/$5/install.progress.txt
+echo "Installing VSTS Build agent package" >> /home/$5/install.progress.txt
 
 # Install VSTS agent
 sudo -u $5 mkdir /home/$5/vsts-agent
@@ -135,11 +184,11 @@ sudo -u $5 tar xzf /home/$5/downloads/vsts-agent-ubuntu*
 echo "LANG=en_US.UTF-8" > .Env
 echo "JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64" >> .Env
 # export JAVA_HOME_6_X64=/usr/lib/jvm/java-6-oracle
-echo "JAVA_HOME_6_X64=/usr/lib/jvm/java-6-openjdk-amd64" >> .Env
+echo "JAVA_HOME_6_X64=/usr/lib/jvm/java-6-oracle" >> .Env
 # export JAVA_HOME_7_X64=/usr/lib/jvm/java-7-oracle
-echo "JAVA_HOME_7_X64=/usr/lib/jvm/java-7-openjdk-amd64" >> .Env
+echo "JAVA_HOME_7_X64=/usr/lib/jvm/java-7-oracle" >> .Env
 # export JAVA_HOME_8_X64=/usr/lib/jvm/java-8-oracle
-echo "JAVA_HOME_8_X64=/usr/lib/jvm/java-8-openjdk-amd64" >> .Env
+echo "JAVA_HOME_8_X64=/usr/lib/jvm/java-8-oracle" >> .Env
 
 # Remove NODE_ENV=production from service template file
 sudo sed -i 's,NODE_ENV=production,,g' ./bin/vsts.agent.service.template
