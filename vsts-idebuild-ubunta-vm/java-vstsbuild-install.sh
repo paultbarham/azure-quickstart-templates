4e334b6b416b35c9cae6b3a25d7f58b51d016798
U-REDMOND\pabarham <pabarham@pbarham-laptop.redmond.corp.microsoft.com>
2016-06-15 15:18:13 -0400

    Added user parameter to svc.sh install in prep for change to agent release

--- vsts-idebuild-ubunta-vm/java-vstsbuild-install.sh
+++ vsts-idebuild-ubunta-vm/java-vstsbuild-install.sh
@@ -210,7 +210,7 @@ echo Running Agent.Listener > /home/$5/install.log.txt 2>&1
 sudo -u $5 -E bin/Agent.Listener --configure --unattended --nostart --replace --acceptteeeula --url $1 --auth PAT --token $2 --pool $3 --agent $4 >> /home/$5/install.log.txt 2>&1
 echo =============================== >> /home/$5/install.log.txt 2>&1
 echo Running ./svc.sh install >> /home/$5/install.log.txt 2>&1
-sudo -E ./svc.sh install >> /home/$5/install.log.txt 2>&1
+sudo -E ./svc.sh install $5 >> /home/$5/install.log.txt 2>&1
 echo =============================== >> /home/$5/install.log.txt 2>&1
 echo Running ./svc.sh start >> /home/$5/install.log.txt 2>&1
 sudo -E ./svc.sh start >> /home/$5/install.log.txt 2>&1
