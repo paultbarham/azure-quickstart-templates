a429e9f851ba12807dc95c00cbe603e1cb04dc53
U-REDMOND\pabarham <pabarham@pbarham-laptop.redmond.corp.microsoft.com>
2016-06-15 08:48:33 -0400

    Changing to two-step run:  call Agent.Listener --configure and then ./run.sh; also setting envs in this script

--- vsts-idebuild-ubunta-vm/java-vstsbuild-install.sh
+++ vsts-idebuild-ubunta-vm/java-vstsbuild-install.sh
@@ -186,22 +186,28 @@ sudo -u $5 tar xzf /home/$5/downloads/vsts-agent-ubuntu*
 # sudo bin/Agent.Listener --unattended --runasservice --replace --acceptteeeula --url $1 --auth PAT --token $2 --pool $3 --agent $4
 
 echo "LANG=en_US.UTF-8" > .env
+export LANG=en_US.UTF-8
 echo "JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64" >> .env
+export JAVA_HOME_6_X64=/usr/lib/jvm/java-6-oracle
 # export JAVA_HOME_6_X64=/usr/lib/jvm/java-6-oracle
 echo "JAVA_HOME_6_X64=/usr/lib/jvm/java-6-oracle" >> .env
+export JAVA_HOME_6_X64=/usr/lib/jvm/java-6-oracle
 # export JAVA_HOME_7_X64=/usr/lib/jvm/java-7-oracle
 echo "JAVA_HOME_7_X64=/usr/lib/jvm/java-7-oracle" >> .env
+export JAVA_HOME_7_X64=/usr/lib/jvm/java-7-oracle
 # export JAVA_HOME_8_X64=/usr/lib/jvm/java-8-oracle
 echo "JAVA_HOME_8_X64=/usr/lib/jvm/java-8-oracle" >> .env
+export JAVA_HOME_8_X64=/usr/lib/jvm/java-8-oracle
 
 # Remove NODE_ENV=production from service template file
 sudo sed -i 's,NODE_ENV=production,,g' ./bin/vsts.agent.service.template
 
 
 # sudo -u $5 -E sh ./config.sh --unattended --runasservice --replace --acceptteeeula --url $1 --auth PAT --token $2 --pool $3 --agent $4
-sudo -u $5 ./config.sh --unattended --runasservice --replace --acceptteeeula --url $1 --auth PAT --token $2 --pool $3 --agent $4 > /home/$5/install.log.txt 2>&1
+# sudo -u $5 ./config.sh --unattended --runasservice --replace --acceptteeeula --url $1 --auth PAT --token $2 --pool $3 --agent $4 > /home/$5/install.log.txt 2>&1
 
-# bin/Agent.Listener --configure --unattended --runasservice --replace --acceptteeeula --url $1 --auth PAT --token $2 --pool $3 --agent $4
+sudo -u $5 -E bin/Agent.Listener --configure --unattended --runasservice --replace --acceptteeeula --url $1 --auth PAT --token $2 --pool $3 --agent $4 > /home/$5/install.log.txt 2>&1
+sudo -u $5 ./run.sh
 
 sudo /bin/date +%H:%M:%S >> /home/$5/install.progress.txt
 echo "ALL DONE!" >> /home/$5/install.progress.txt
